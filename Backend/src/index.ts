65605893891bec26316d441d4d268cf280159fd3
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-09 17:01:19 +0300

    Works most ly now moving to testing the error handling platform

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,15 +1,29 @@
 import "dotenv/config"
+import cors from "cors"
 import express from "express";
 import connectToDatabase from "./config/db";
-import { NODE_ENV, PORT } from "./constants/env";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import cookieParser from "cookie-parser";
+import errorHandler from "./middleware/errorHandler"
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin:APP_ORIGIN,
+  credentials: true
+}))
+
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "healthy",
   });
 });
+
+app.use(errorHandler)
 
 app.listen(4004, async () => {
   console.log(`Server is running on port ${PORT} in the ${NODE_ENV} environment`);
