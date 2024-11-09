c18cdefb4fb5fc45406a008bdb205331af8cfd78
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-11-09 17:18:34 +0300

    Updated the calls to make them human readables

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,31 +1,37 @@
-import "dotenv/config"
-import cors from "cors"
+import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import connectToDatabase from "./config/db";
 import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
 import cookieParser from "cookie-parser";
-import errorHandler from "./middleware/errorHandler"
+import errorHandler from "./middleware/errorHandler";
+import catchErrors from "./utils/catchErrors";
+import { OK } from "./constants/http";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin:APP_ORIGIN,
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: APP_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
+app.get("/", (req, res, next) => {
+  res.status(OK).json({
     status: "healthy",
   });
 });
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(4004, async () => {
-  console.log(`Server is running on port ${PORT} in the ${NODE_ENV} environment`);
-  await connectToDatabase()
+  console.log(
+    `Server is running on port ${PORT} in the ${NODE_ENV} environment`
+  );
+  await connectToDatabase();
 });
